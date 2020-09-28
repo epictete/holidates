8a39523761b800d391c5cc6a166d6e2b1845498e
epictete <john.laterre@gmail.com>
2020-09-28 17:14:23 +0200

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# nager-date
-npm CLI tool taht returns a list of holidays dates for the current year
+# Holidates
+
+CLI tool that returns a list of holidays dates for a specified country/year.
+
+### npm
+
+```
+npx @epictete/holidates
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const { getCode } = require("country-list");
+const axios = require("axios");
+
+const args = process.argv.slice(2);
+const country = getCode(args[0]);
+const year = args[1] ? args[1] : new Date().getFullYear();
+
+axios
+  .get(`https://date.nager.at/Api/v1/Get/${country}/${year}`)
+  .then(response => handleResponse(response.data))
+  .catch(err => console.log(err));
+
+handleResponse = holidays => {
+  holidays.map(holiday => {
+    console.log(`Holiday: ${holiday.name}\nDate: ${holiday.date}\n`);
+  });
+};
